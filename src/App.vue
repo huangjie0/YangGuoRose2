3a45a857dacefd70b6a4f3a20c537331136587f7
huangjie4 <1364045672@qq.com>
2024-04-09 15:35:15 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@
 </script>
 
 <template>
-    <el-row>
-      <el-button>默认按钮</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-    </el-row>
- 
+    <router-view></router-view>
 </template>
 
 <style scoped lang="less">
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,26 @@
 import { createRouter,createWebHashHistory } from 'vue-router'
+import Index from '@/pages/Index.vue'
+import About from '@/pages/About.vue'
+import NotFound from '@/pages/404.vue'
 
-const routes:any[] = []
+const routes:any[] = [
+    {
+        path:"/",
+        component:Index
+    },
+    {
+        path:'/about',
+        component:About
+    },
+    {
+        path:'/about',
+        component:About
+    },
+    { 
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound },
+]
 
 const router = createRouter({
     history: createWebHashHistory(),

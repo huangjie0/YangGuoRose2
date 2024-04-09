d0308298695ce03fe465c0698ee1ce7ca0feefda
huangjie4 <1364045672@qq.com>
2024-04-09 16:01:07 +0800

    修改

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router'
+import "./style.less"
+import "./elementui.less"
 
 const app = createApp(App)
 

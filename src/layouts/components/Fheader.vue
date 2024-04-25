18cb062a66aa96beb3182eb2e6ed6e7665e72b43
huangjie4 <1364045672@qq.com>
2024-04-25 15:57:41 +0800

    修改

--- src/layouts/components/Fheader.vue
+++ src/layouts/components/Fheader.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="rose-f-row f-header rose-bg-p rose-bg4 rose-f-fixed">
-        <span>
-            <el-icon><Bicycle /></el-icon>
-            洋果子Rose后台系统
-            <el-icon><Fold /></el-icon>
-            <el-icon><Refresh /></el-icon>
+        <span class="f-header-logo rose-f-c rose-font-s3">
+            <el-icon class="rose-mr-1"><Bicycle /></el-icon>
+            <span class="rose-mr-1">洋果子Rose后台系统</span>
+            <el-icon class="rose-mr-1 rose-cursor"><Fold /></el-icon>
+            <el-icon class="rose-mr-1 rose-cursor"><Refresh /></el-icon>
         </span>
-        <div>
-            <el-icon><FullScreen /></el-icon>
-            <el-dropdown>
+        <div class="rose-ml-a rose-f-row logo-right">
+            <el-icon class="rose-cursor rose-mr-1"><FullScreen /></el-icon>
+            <el-dropdown class="dropdown rose-bg4 rose-mr-1">
                 <span class="el-dropdown-link">
-                <el-avatar :size="25" :src="userStore.user.avatar" />
-                {{ userStore.user.username }}
-                <el-icon class="el-icon--right">
+                <el-avatar :size="25" :src="userStore.user?.avatar" />
+                {{ userStore.user?.username }}
+                <el-icon>
                     <arrow-down />
                 </el-icon>
                 </span>
@@ -38,5 +38,13 @@ const userStore = useUserStore()
     top:0;
     left:0;
     right:0;
+    align-items: center;
+    &-logo{
+        width: 250px;
+    }
+    .logo-right{
+        align-items: center;
+    }
+
 }
 </style>

ccadcb46699b7fe22f5f114192d2e6d05c844ac7
huangjie4 <1364045672@qq.com>
2024-05-30 16:42:54 +0800

    修改

--- src/layouts/components/Fmenu.vue
+++ src/layouts/components/Fmenu.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="f-menu rose-f-fixed" :style="{ width: screenStore.asideWidth }">
     <el-scrollbar>
-      <el-menu default-active="2" @select="handleSelect" 
-      :collapse="screenStore.asideWidth == '64px' ? true : false">
-        <template v-for="item in asideMenus" :key="item.icon">
+      <el-menu active-text-color="#a781ee" :default-active="defaultActive" @select="handleSelect" 
+      :collapse="isCollapse" :unique-opened="false" :collapse-transition="false">
+        <template v-for="(item,index) in asideMenus">
           <el-sub-menu
             :index="item.name"
+            :key="index"
             v-if="item.child && item.child.length > 0"
           >
             <template #title>
@@ -33,15 +34,28 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { useRouter } from "vue-router";
+import { ref,computed } from "vue";
+import { useRouter,useRoute, onBeforeRouteUpdate } from "vue-router";
 import useScreenStore from "@/store/screen.ts";
 
 const router = useRouter()
+const route = useRoute()
 const screenStore = useScreenStore()
+
+//默认激活状态下的路由路劲
+const defaultActive = ref(route.path)  
 
 const handleSelect = (path:string)=>{
     router.push(path)
 }
+
+const isCollapse = computed(()=>{
+    return screenStore.asideWidth == '64px' ? true : false 
+})
+
+onBeforeRouteUpdate((to) => {
+    defaultActive.value = to.path;
+});
 
 const asideMenus: any[] = [
   {
@@ -186,8 +200,10 @@ const asideMenus: any[] = [
 </script>
 <style lang="less" scoped>
 .f-menu {
-  top: 64px;
-  bottom: 0;
-  left: 0;
+    transition:all 0.2s;
+    top: 64px;
+    bottom: 0;
+    left: 0;
+    background-color: var(--rose-w);
 }
 </style>

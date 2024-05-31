01769178580171284cfa3b66fafccbeb75a1d236
huangjie4 <1364045672@qq.com>
2024-05-31 09:49:44 +0800

    修改

--- src/layouts/components/Fmenu.vue
+++ src/layouts/components/Fmenu.vue
@@ -37,10 +37,12 @@
 import { ref,computed } from "vue";
 import { useRouter,useRoute, onBeforeRouteUpdate } from "vue-router";
 import useScreenStore from "@/store/screen.ts";
+import useUserStore from "@/store/index.ts";
 
 const router = useRouter()
 const route = useRoute()
 const screenStore = useScreenStore()
+const userStore = useUserStore()
 
 //默认激活状态下的路由路劲
 const defaultActive = ref(route.path)  
@@ -57,146 +59,10 @@ onBeforeRouteUpdate((to:any) => {
     defaultActive.value = to.path;
 });
 
-const asideMenus: any[] = [
-  {
-    name: "后台面板",
-    icon: "help",
-    child: [
-      {
-        name: "主控台",
-        icon: "home-filled",
-        frontpath: "/",
-      },
-    ],
-  },
-  {
-    name: "商城管理",
-    icon: "shopping-bag",
-    child: [
-      {
-        name: "商品管理",
-        icon: "shopping-cart-full",
-        frontpath: "/goods/list",
-      },
-      {
-        name: "分类管理",
-        icon: "menu",
-        frontpath: "/category/list",
-      },
-      {
-        name: "规格管理",
-        icon: "aim",
-        frontpath: "/skus/list",
-      },
-      {
-        name: "优惠券管理",
-        icon: "postcard",
-        frontpath: "/coupon/list",
-      },
-    ],
-  },
-  {
-    name: "用户管理",
-    icon: "user",
-    child: [
-      {
-        name: "用户管理",
-        icon: "user-filled",
-        frontpath: "/user/list",
-      },
-      {
-        name: "会员等级",
-        icon: "data-analysis",
-        frontpath: "/level/list",
-      },
-    ],
-  },
-  {
-    name: "订单管理",
-    icon: "message-box",
-    child: [
-      {
-        name: "订单管理",
-        icon: "reading",
-        frontpath: "/order/list",
-      },
-      {
-        name: "评论管理",
-        icon: "comment",
-        frontpath: "/comment/list",
-      },
-    ],
-  },
-  {
-    name: "管理员管理",
-    icon: "management",
-    child: [
-      {
-        name: "管理员管理",
-        icon: "coordinate",
-        frontpath: "/manager/list",
-      },
-      {
-        name: "权限管理",
-        icon: "coordinate",
-        frontpath: "/access/list",
-      },
-      {
-        name: "角色管理",
-        icon: "histogram",
-        frontpath: "/role/list",
-      },
-    ],
-  },
-  {
-    name: "系统设置",
-    icon: "setting",
-    child: [
-      {
-        name: "基础设置",
-        icon: "baseball",
-        frontpath: "/setting/base",
-      },
-      {
-        name: "交易设置",
-        icon: "credit-card",
-        frontpath: "/setting/buy",
-      },
-    ],
-  },
-  {
-    name: "分销模块",
-    icon: "shopping-cart",
-    child: [
-      {
-        name: "分销员管理",
-        icon: "user-filled",
-        frontpath: "/distribution/index",
-      },
-      {
-        name: "分销设置",
-        icon: "edset-up",
-        frontpath: "/distribution/setting",
-      },
-    ],
-  },
-  {
-    name: "其他模块",
-    icon: "smostly-cloudy",
-    child: [
-      {
-        name: "图库管理",
-        icon: "picture-filled",
-        frontpath: "/image/list",
-      },
-      {
-        name: "公告管理",
-        icon: "notification",
-        frontpath: "/notice/list",
-      },
-    ],
-  },
-];
+const asideMenus = computed(()=>{
+    return userStore.menus
+})
+
 </script>
 <style lang="less" scoped>
 .f-menu {

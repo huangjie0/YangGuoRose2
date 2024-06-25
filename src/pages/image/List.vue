fb8e19397fab7afe2a7d0ef77d9cc489364489e0
huangjie4 <1364045672@qq.com>
2024-06-25 11:04:09 +0800

    修改

--- src/pages/image/List.vue
+++ src/pages/image/List.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
         <el-container class="rose-bg-w rose-br-s1" :style="{ height:h + 'px' }">
-            <el-header>Header</el-header>
+            <el-header class="rose-header rose-f-row">
+                header
+            </el-header>
             <el-container>
-                <el-aside width="200px">
-                    <div class="top">
-                        <div v-for="i in 100" :key="i"> 
-                            {{ i }}
+                <el-aside width="200px" class="rose-aside rose-p-r">
+                    <div class="rose-aside-top rose-p-a-0">
+                        <el-scrollbar>
+                            <div v-for="i in 100" :key="i"> 
+                                {{ i }}
+                            </div>
+                        </el-scrollbar>
+                    </div>
+                    <div class="rose-aside-bottom rose-p-a-02 rose-f-c ">
+                        <div>
+                            分页区域dsfgdffdgfgfgfdg
                         </div>
                     </div>
                 </el-aside>
@@ -22,5 +31,18 @@ const h:number = windowHeight - 144;
 
 </script>
 <style lang="less" scoped>
+.rose-header{
+    border-bottom: 1px solid var(--rose-g3);
+    align-items: center;
+}
 
+.rose-aside{
+    border-right: 1px solid var(--rose-g3);
+    &-top{
+        bottom: var(--common-split8);
+    }
+    &-bottom{
+        height: var(--common-split8);
+    }
+}
 </style>

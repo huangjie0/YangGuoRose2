3d29105ecc622de2028c6fb201edaead31940ed0
huangjie4 <1364045672@qq.com>
2024-06-28 14:53:25 +0800

    修改

--- src/components/ImageAside.vue
+++ src/components/ImageAside.vue
@@ -2,14 +2,20 @@
     <el-aside width="200px" class="rose-aside rose-p-r">
         <div class="rose-aside-top rose-p-a-0">
             <el-scrollbar>
-                <div v-for="i in 100" :key="i"> 
-                    {{ i }}
+                <div class="rose-aside-list">
+                    <span>分类标题</span>
+                    <el-button style="margin-left: auto;" text type="primary" size="small">
+                        <el-icon text :size="12"><Edit/></el-icon>
+                    </el-button>
+                    <el-button type="danger" size="small">
+                        <el-icon :size="12"><Close/></el-icon>
+                    </el-button>
                 </div>
             </el-scrollbar>
         </div>
         <div class="rose-aside-bottom rose-p-a-02 rose-f-c ">
             <div>
-                分页区域dsfgdffdgfgfgfdg
+                分页区域
             </div>
         </div>
     </el-aside>
@@ -26,6 +32,14 @@
     }
     &-bottom{
         height: var(--common-split8);
+    }
+    &-list{
+        border-bottom: 1px solid var(--rose-g3);
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        color: var(--rose-g4);
+        cursor: pointer;
     }
 }
 </style>

9000d5862d66ba93e074c6c78adea95570cba50c
huangjie4 <1364045672@qq.com>
2024-07-01 17:22:29 +0800

    分页器相关的配置bug

--- src/components/ImageMain.vue
+++ src/components/ImageMain.vue
@@ -2,7 +2,7 @@
     <el-main class="rose-p-r">
         <div class="rose-p-a-0 rose-main-bottom">
             <el-scrollbar>
-                
+                111  
             </el-scrollbar>
         </div>
     </el-main>

6daa1122b6a1b8b0e62e95173acf9b63571cc479
huangjie4 <1364045672@qq.com>
2024-07-01 16:44:28 +0800

    分页器相关功能开发

--- src/components/ImageAside.vue
+++ src/components/ImageAside.vue
@@ -1,26 +1,42 @@
 <template>
-    <el-aside width="200px" class="rose-aside rose-p-r">
+    <el-aside width="200px" class="rose-aside rose-p-r" v-loading="loading">
         <div class="rose-aside-top rose-p-a-0">
             <el-scrollbar>
-                <AsideList>分类标题</AsideList>
-                <AsideList active>分类标题</AsideList>
+                <AsideList :active="activeId == item.id" v-for="(item,index) in imageList" :key="index">{{ item.name }}</AsideList>
             </el-scrollbar>
         </div>
-        <div class="rose-aside-bottom rose-p-a-02 rose-f-c ">
-            <div>
-                分页区域
-            </div>
+        <div class="rose-aside-bottom rose-p-a-02 rose-f-c">
+            <el-pagination background layout="prev,next" :total="total" :current-page="currentPage" :page-size="pageSize" @current-change="getImageList"/>
         </div>
     </el-aside>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
 import { getImageClassList } from '@/api/imageClass.ts'
 import AsideList from '@/components/AsideList.vue'
 
+const loading = ref(false)
+const imageList = ref<any[]>([])
+const activeId = ref(0)
+
+//分页
+const currentPage = ref(1)
+const total = ref(0)
+const pageSize = ref(10)
+
 //获取图片列表数据
-const getImageList = ()=>{
-    getImageClassList(1).then((res:any)=>{
-        console.log(res);
+const getImageList = (p:any = null)=>{
+    (typeof p == "number") && (currentPage.value = p)
+
+    loading.value = true
+    getImageClassList(currentPage.value).then((res:any)=>{
+        imageList.value = res.list
+        total.value = res.totalCount
+        
+        let item = imageList.value[0]
+        item && (activeId.value = item.id)
+    }).finally(()=>{
+        loading.value = false
     })
 }
 

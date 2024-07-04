dc80e984a67dece56d8df02361c72bd008071904
huangjie4 <1364045672@qq.com>
2024-07-04 15:36:13 +0800

    修改

--- src/components/ImageMain.vue
+++ src/components/ImageMain.vue
@@ -1,16 +1,13 @@
 <template>
-    <el-main class="rose-p-r">
-        <div class="rose-p-a-0 rose-main-bottom">
-            <el-scrollbar>
-                111
-            </el-scrollbar>
-        </div>
-    </el-main>
-
+  <el-main class="rose-p-r">
+    <div class="rose-p-a-0 rose-main-bottom">
+      <el-scrollbar>
+        11
+      </el-scrollbar>
+    </div>
+  </el-main>
 </template>
 <script setup lang="ts">
 
 </script>
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>

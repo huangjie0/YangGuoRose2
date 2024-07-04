3227a5763f50557871acc17133bd48c7bb74396e
huangjie4 <1364045672@qq.com>
2024-07-04 09:11:14 +0800

    修改

--- src/components/ImageMain.vue
+++ src/components/ImageMain.vue
@@ -2,7 +2,7 @@
     <el-main class="rose-p-r">
         <div class="rose-p-a-0 rose-main-bottom">
             <el-scrollbar>
-                111  
+                111
             </el-scrollbar>
         </div>
     </el-main>
@@ -12,7 +12,5 @@
 
 </script>
 <style lang="less" scoped>
-.rose-main-bottom{
-    bottom: 0;
-}
+
 </style>

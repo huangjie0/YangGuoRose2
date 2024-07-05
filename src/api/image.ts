b8aa4f97f5fd595548a6cebe72fd711a2e2337d2
huangjie4 <1364045672@qq.com>
2024-07-05 13:42:58 +0800

    修改

--- src/api/image.ts
+++ src/api/image.ts
@@ -1,1 +1,6 @@
-import axios from '@/axios.ts'
+import axios from '@/axios.ts';
+
+
+export function getImageList(id:number,page:number=1){
+    return axios.get(`/admin/image_class/${id}/image/${page}`)
+}
--- src/components/ImageMain.vue
+++ src/components/ImageMain.vue
@@ -2,12 +2,13 @@
   <el-main class="rose-p-r">
     <div class="rose-p-a-0 rose-main-bottom">
       <el-scrollbar>
-        11
+
       </el-scrollbar>
     </div>
   </el-main>
 </template>
 <script setup lang="ts">
+// import { getImageList } from '@/api/image.ts';
 
 </script>
 <style lang="less" scoped></style>
